8c0bfe78c48384767bcd9671550412176cef2ba7
Paul Horikx <p.horikx@gmail.com>
2023-11-06 22:44:16 +0100

    switched to local cali fork

--- grpc_backend/web/src/entry/main.rs
+++ grpc_backend/web/src/entry/main.rs
@@ -2,6 +2,7 @@ use cali_derive::setup_server;
 use grpc_backend_web::config::Config;
 use std::sync::Arc;
 use std::{error::Error, str::FromStr};
+use tonic::{Request, Status};
 
 #[derive(Debug, Clone)]
 pub struct ServerContext {}
@@ -10,7 +11,6 @@ pub struct ServerContext {}
 async fn main() -> Result<(), Box<dyn Error>> {
     let my_context = Arc::new(ServerContext {});
     setup_server!("grpc_backend", "0.1.1", my_context);
-    cali_core::logging::util::setup();
-    log::info!("Started server");
     Ok(())
 }
+
